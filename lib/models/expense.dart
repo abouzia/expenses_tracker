19d56dec25a984d11e0160bb7807d8e5c4c4cc29
Yassine <yassine.abouzia@gmail.com>
2023-11-09 15:15:27 +0100

    styled expenses list

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
+final formatter = DateFormat.yMd();
 const Uuid uuid = Uuid();
 
 enum Category { food, work, leisure, travel }
+
+const CategoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.work: Icons.work,
+  Category.leisure: Icons.movie,
+  Category.travel: Icons.flight_takeoff
+};
 
 class Expense {
   Expense({
@@ -17,4 +27,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
